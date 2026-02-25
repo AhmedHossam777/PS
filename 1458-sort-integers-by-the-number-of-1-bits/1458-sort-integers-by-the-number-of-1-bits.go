7eaf49dec353ccd0bc7369e77d70fedb44172d68
Ahmed Hossam <ahmedhossam9226@gmail.com>
2026-02-25 02:47:16 +0200

    1458-SORT-INTEGERS-BY-THE-NUMBER-OF-1-BITS Stats: Time: 4 ms (24.24%), Space: 5.3 MB (84.85%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.go
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.go
@@ -1,27 +1,13 @@
-func findNumberOfOnes(num int) int {
-	binaryStr := strconv.FormatInt(int64(num), 2)
-	numOfOnes := 0
-
-	for i := 0; i < len(binaryStr); i++ {
-		if binaryStr[i] == 49 {
-			numOfOnes++
-		}
-	}
-	return numOfOnes
-}
-
 func sortByBits(arr []int) []int {
-	sort.Slice(
-		arr, func(i, j int) bool {
-			onesI := findNumberOfOnes(arr[i])
-			onesJ := findNumberOfOnes(arr[j])
-
-			if onesI == onesJ {
-				return arr[i] < arr[j]
-			}
-			return onesI < onesJ
-		},
-	)
-
+	sort.Slice(arr, func(i, j int) bool {
+		onesI := bits.OnesCount(uint(arr[i]))
+		onesJ := bits.OnesCount(uint(arr[j]))
+		
+		if onesI == onesJ {
+			return arr[i] < arr[j]
+		}
+		return onesI < onesJ
+	})
+	
 	return arr
 }
