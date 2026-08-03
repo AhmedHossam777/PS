b877fc80d855b5b70ec2839f2e7fabf0b1482031
Ahmed Hossam <ahmedhossam9226@gmail.com>
2026-08-03 22:09:17 +0300

    3859-MAXIMUM-PRODUCT-OF-TWO-DIGITS Stats: Time: 1 ms (100%), Space: 42.8 MB (35.58%) - LeetHub

--- 3859-maximum-product-of-two-digits/3859-maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/3859-maximum-product-of-two-digits.java
@@ -1,21 +1,26 @@
 class Solution {
   public int maxProduct(int n) {
-    String str = Integer.toString(n);
+   String str = Integer.toString(n);
 		int res = 1;
 		int max = 0;
+		int first = 0;
+		int second = 0;
 		if (str.length() < 3) {
 			for (int i = 0; i < str.length(); i++) {
 				res *= Character.getNumericValue(str.charAt(i));
 			}
-      max = res;
+			max = res;
 		} else {
 			for (int i = 0; i < str.length(); i++) {
-				for (int j = 0; j < str.length(); j++) {
-					if (i == j) continue;
-					res = Character.getNumericValue(str.charAt(i)) * Character.getNumericValue(str.charAt(j));
-					if (max < res) max = res;
+				if (Character.getNumericValue(str.charAt(i)) > first || Character.getNumericValue(str.charAt(i)) > second) {
+					if (first < second) {
+						first = Character.getNumericValue(str.charAt(i));
+					} else {
+						second = Character.getNumericValue(str.charAt(i));
+					}
 				}
 			}
+			max = first * second;
 		}
 		return max;
   }
